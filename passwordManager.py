5a9fdbf115fe78c903a8efddcb78e7adab3232fe
subhankarray <subhankarraypp32@gmail.com>
2023-06-17 15:18:56 +0530

    Password Manager: V.1.1.1

--- passwordManager.py
+++ passwordManager.py
@@ -3,32 +3,65 @@ import csv
 import os
 from getpass import getpass
 
-filePath="D:\\Password.txt"
+
 passkeep=["Website","Password","Level"]
 
 class File:
 
- def __init__(self,web,pasw,lvl):
+ def __init__(self):
   self.objE=Encrypt()
   self.website={"Website":"","Password":"","Level":-1}
-  self.website["Website"]=web
-  
-  self.objE.lockText(pasw,lvl)
-  
-  self.website["Password"]=self.objE.psw
-  self.website["Level"]=lvl
-  print(self.website)
 
- def read(self):
-  return;
 
- def write(self):
+ def dictInitialise(self,web,pasw,lvl,ch):
+  if(ch=="w"):
+   self.website["Website"]=web
+  
+   self.objE.lockText(pasw,lvl)
+  
+   self.website["Password"]=self.objE.psw
+   self.website["Level"]=lvl
+  elif(ch=="r"):
+   self.website["Website"]=web
+  else:
+   self.website["Website"]=web
+
+ def read(self,filePath):
+  with open(filePath,"r") as file:
+    csvReader=csv.DictReader(file,fieldnames=passkeep)
+    for row in csvReader:
+      if(row["Website"]==self.website["Website"]):
+        self.objE.psw=row["Password"]
+        self.objE.unlockText(int(row["Level"]))
+    return
+
+ def write(self,filePath):
   with open(filePath,"a") as file:
     csvWriter=csv.DictWriter(file,fieldnames=passkeep)
     csvWriter.writerow(self.website) 
     
+ def filterData(self,obj):
+  pathManager().unHideFiles("D:\\Password.txt")
+  obj.delete("D:\\Password.txt","D:\\tempFile.txt")
+  obj.delete("D:\\tempFile.txt","D:\\Password.txt")
+  pathManager().hideFile("D:\\Password.txt")
  
- def erase(self):
+ def delete(self,filePath,tempPath):
+  with open(filePath,"r") as file: 
+   csvReader=csv.DictReader(file,fieldnames=passkeep)
+   for row in csvReader:
+    self.erase(tempPath)
+    if(row["Website"]!=self.website["Website"]):
+     temp=self.website["Website"]
+
+     self.website["Website"]=row["Website"]
+     self.website["Password"]=row["Password"]
+     self.website["Level"]=row["Level"]
+     self.write(tempPath)
+     
+     self.website["Website"]=temp 
+ 
+ def erase(self,filePath):
   with open(filePath,"w") as file:
     csvWriter=csv.DictWriter(file,fieldnames=passkeep)
     csvWriter.writeheader()
@@ -42,10 +75,9 @@ class Encrypt:
  def lockText(self,u,key):
   for i in u:
     self.psw=self.psw+chr(ord(i)+key)
-  print("LT",self.psw,end="\n")
 
  def unlockText(self,key):
-  print("Display:",end="")
+  print("Password:",end="")
   for i in self.psw:
     print(chr(ord(i)-key),end="")
   print()
@@ -55,15 +87,39 @@ class pathManager:
  def __init__(self):
   return
 
- def isFileExist(self,obj):
+ def isFileExist(self,obj,filePath):
   if (not os.path.exists(filePath)):
-            obj.erase()
-            obj.write()
+            obj.erase(filePath)
+            obj.write(filePath)
   else:
-            obj.write()
-  
+            obj.write(filePath)
+
+ def remTempFile(self,filePath):
+  return
+
+ def hideFile(self,filePath):
+  p=os.system("attrib +h "+filePath)
+  return
+
+ def unHideFiles(self,filePath):
+  p=os.system("attrib -h "+filePath)
+  return
+ 
+   
 def main():
- obj=File(input("Website:"),getpass(),int(input("Level:")))
- pathManager().isFileExist(obj)
+ pathManager().hideFile("D:\\Password.txt")
+ choice=sys.argv[1]
+ obj=File()
+ if(choice=="w"):
+  obj.dictInitialise(input("Website:"),getpass(),int(input("Level:")),choice)
+  pathManager().isFileExist(obj,"D:\\Password.txt")
+ elif(choice=="r"):
+  obj.dictInitialise(input("Website:"),"",0,choice)
+  obj.read("D:\\Password.txt")
+ else:
+  obj.dictInitialise(input("Website:"),"",0,choice)
+  obj.filterData(obj)
+  
+   
 
 main()
