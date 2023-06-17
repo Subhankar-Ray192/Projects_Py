a3e6e8a9b6cc2633aa98c3c604917e1b74dde290
subhankarray <subhankarraypp32@gmail.com>
2023-06-17 22:31:25 +0530

    Password Manager: V.1.2.3

--- passwordManager.py
+++ passwordManager.py
@@ -11,62 +11,91 @@ class File:
  def __init__(self):
   self.objE=Encrypt()
   self.website={"Website":"","Password":"","Level":-1}
+  self.updateWebsite={"Website":"","Password":"","Level":-1}
 
 
  def dictInitialise(self,web,pasw,lvl,ch):
   if(ch=="-w"):
-   self.website["Website"]=web
+   self.website[passkeep[0]]=web
   
    self.objE.lockText(pasw,lvl)
   
-   self.website["Password"]=self.objE.psw
-   self.website["Level"]=lvl
-  elif(ch=="-r"):
-   self.website["Website"]=web
+   self.website[passkeep[1]]=self.objE.psw
+   self.website[passkeep[2]]=lvl
+  elif(ch=="-u"):
+   self.updateWebsite[passkeep[0]]=web
+  
+   self.objE.lockText(pasw,lvl)
+  
+   self.updateWebsite[passkeep[1]]=self.objE.psw
+   self.updateWebsite[passkeep[2]]=lvl
   else:
-   self.website["Website"]=web
+   self.website[passkeep[0]]=web
 
  def read(self,filePath):
   with open(filePath,"r") as file:
     csvReader=csv.DictReader(file,fieldnames=passkeep)
     for row in csvReader:
-      if(row["Website"]==self.website["Website"]):
-        self.objE.psw=row["Password"]
-        self.objE.unlockText(int(row["Level"]))
-    return
+      if(row[passkeep[0]]==self.website[passkeep[0]]):
+        self.objE.psw=row[passkeep[1]]
+        self.objE.unlockText(int(row[passkeep[2]]))
+
+ def updateRecord(self,filePath):
+  with open(filePath,"r") as file:
+   csvReader=csv.DictReader(file,fieldnames=passkeep)
+   for row in csvReader:
+    if(row[passkeep[0]]==self.updateWebsite[passkeep[0]]):
+      self.filterData(filePath,"D:\\tempFile.txt")
+      self.website[passkeep[0]]=self.updateWebsite[passkeep[0]]
+      self.website[passkeep[1]]=self.updateWebsite[passkeep[1]]
+      self.website[passkeep[2]]=self.updateWebsite[passkeep[2]]
+      self.write(filePath)
+      
+ 
+ def showWeb(self,filePath):
+  with open(filePath,"r") as file:
+   csvReader=csv.DictReader(file,fieldnames=passkeep)
+   for row in csvReader:
+     if(row[passkeep[0]]!=passkeep[0]):
+       print(row[passkeep[0]],end="\n")
 
  def write(self,filePath):
-  with open(filePath,"a") as file:
+  with open(filePath,"a",newline="",encoding="utf-8") as file:
     csvWriter=csv.DictWriter(file,fieldnames=passkeep)
     csvWriter.writerow(self.website) 
     
- def filterData(self,obj,filePath,tempPath):
+ def filterData(self,filePath,tempPath):
   pathManager().unHideFiles(filePath)
-  obj.delete(filePath,tempPath)
-  obj.delete(tempPath,filePath)
+  self.delete(filePath,tempPath)
+  self.delete(tempPath,filePath)
   pathManager().remTempFile(tempPath)
   pathManager().hideFile(filePath)
  
  def delete(self,filePath,tempPath):
   with open(filePath,"r") as file: 
    csvReader=csv.DictReader(file,fieldnames=passkeep)
+   self.erase(tempPath)
    for row in csvReader:
-    self.erase(tempPath)
-    if(row["Website"]!=self.website["Website"]):
-     temp=self.website["Website"]
+    if((row[passkeep[0]]!=self.website[passkeep[0]])and(self.isHeader(row))):
+     temp=self.website[passkeep[0]]
 
-     self.website["Website"]=row["Website"]
-     self.website["Password"]=row["Password"]
-     self.website["Level"]=row["Level"]
+     self.website[passkeep[0]]=row[passkeep[0]]
+     self.website[passkeep[1]]=row[passkeep[1]]
+     self.website[passkeep[2]]=row[passkeep[2]]
      self.write(tempPath)
      
      self.website["Website"]=temp 
  
+ def isHeader(self,row):
+  if((row[passkeep[0]]==passkeep[0])or((row[passkeep[1]]==passkeep[1]))or((row[passkeep[2]]==passkeep[2]))):
+   return False
+  return True
+ 
  def erase(self,filePath):
-  with open(filePath,"w") as file:
+  with open(filePath,"w",newline="",encoding="utf-8") as file:
     csvWriter=csv.DictWriter(file,fieldnames=passkeep)
     csvWriter.writeheader()
-    csvWriter.writerow({"Website":"Master","Password":"5322","Level":1})
+    #csvWriter.writerow({"Website":"Master","Password":"5322","Level":1})
 
 class Encrypt:
 
@@ -118,18 +147,20 @@ class PasswordMan:
    self.view()
   elif(choice.__eq__("-f")):
    self.remove()
+  elif(choice.__eq__("-l")):
+   self.listingWeb()
+  elif(choice.__eq__("-u")):
+   self.update()
   else:
    help()
   return
 
  def store(self):
-  print(1)
   self.objF.dictInitialise(input("Website:"),getpass(),int(input("Level:")),"-w")
   self.objP.isFileExist(self.objF,"D:\\Password.txt")
   self.objP.hideFile("D:\\Password.txt")
 
  def view(self):
-  print(2)
   self.objF.dictInitialise(input("Website:"),"",0,"-r")
   self.objF.read("D:\\Password.txt")
   self.objP.hideFile("D:\\Password.txt")
@@ -138,8 +169,17 @@ class PasswordMan:
  def remove(self):
   print(3)
   self.objF.dictInitialise(input("Website:"),"",0,"-f")
-  self.objF.filterData(self.objF,"D:\\Password.txt","D:\\tempFile.txt")
-  return  
+  self.objF.filterData("D:\\Password.txt","D:\\tempFile.txt")
+  return
+
+ def listingWeb(self):
+  self.objF.showWeb("D:\\Password.txt")
+  return 
+
+ def update(self):
+  self.objF.dictInitialise(input("Website:"),getpass(),int(input("Level:")),"-u")
+  self.objF.updateRecord("D:\\Password.txt") 
+  return
      
 def main():
  print(sys.argv[1])
