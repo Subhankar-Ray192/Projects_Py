66c199e586b9d031f5284089451ddc64e729716d
subhankarray <subhankarraypp32@gmail.com>
2023-06-28 03:48:12 +0530

    Chrome History Analyzer: V.1.1.2
    Visual Graph Added

--- webHistoryReader.py
+++ webHistoryReader.py
@@ -6,10 +6,14 @@ import sys
 import urllib.parse
 from datetime import datetime,timedelta
 import matplotlib.pyplot as plot
+import math
 
 
 
-domainExtensions = ["com","org",""]
+domainExtensions = ["com","org","net","co","us","edu",""]
+protocol = ["https","ftp","http","smtp",""]
+domainName=["google","youtube","facebook","instagram","whatsapp",""]
+
 queryTitle = "'%%'" 
 
 dayFactor = 365
@@ -75,13 +79,12 @@ class Query:
    for i in webList:
      self.queryUrl.append("'%{}%'".format(i))
   
-  def clearQuery(self):
-   for i in range(len(self.queryUrl)):
-     self.queryUrl.pop()
-  
   def showQuery(self):
    for i in self.queryUrl:
      print(i)
+  
+  def constData(self):
+   return len(self.queryUrl),self.fetchQuery(len(self.queryUrl)-1)
   
   def sqlQuery(self,time_from,time_to,title,url):
    
@@ -111,28 +114,60 @@ class Query:
     #print(f"{len(fetch_result)} results...")
     return len(fetch_result)
     
+
+class Visualize:
  
+ def __init(self):
+  return
+ 
+ def drawPieChart(self,dataset,labelSet):
+  plot.pie(dataset, labels = labelSet)
+  plot.legend(title="domain_extension")
+  plot.show()
+ 
+class Statistics:
+ 
+  def __init__(self):
+   self.objQ= Query()
+   self.objV= Visualize()
+   return
+  
+  def staticQueryDetails(self):
+   self.objQ.showQuery()
+   print(f"Query chrome history")
+   print(f"From: {self.objQ.objT.dtToString(dt_From)}, To: {self.objQ.objT.dtToString(dt_To)}")
+   print(f"Title: --")
+  
+  def dynamicQueryDetails(self,index,label):
+   print(f"url: {label[index]}")
+    
+  def genDataSet(self,header):
+   specific_result = []
+   self.objQ.assignQuery(header)
+   
+   sum=0
+   count,total_result = self.objQ.constData()
+   label = header[0:count-1]
+   label.append("others")
+   
+   for i in range(count-1):
+     self.dynamicQueryDetails(i,label)
+     sum = sum+self.objQ.fetchQuery(i)
+     specific_result.append((self.objQ.fetchQuery(i)//total_result)*100)    
+   
+   self.dynamicQueryDetails(count-1,label)
+   specific_result.append(1)  
+   self.objV.drawPieChart(specific_result,label)
 
 def main():
- objQ=Query()
- objQ.assignQuery(domainExtensions)
- objQ.showQuery()
- print(f"Query chrome history")
- print(f"From: {objQ.objT.dtToString(dt_From)}, To: {objQ.objT.dtToString(dt_To)}")
- print(f"Title {queryTitle}")
- print(f"Url {domainExtensions[0]}")
-
- total_result=objQ.fetchQuery(2)
-
- specific_result=objQ.fetchQuery(0)
-
- percent=(specific_result/total_result)*100
-
- print("Percent:{0:.2f}%".format(percent))
  
- k=[20,30,50]
- plot.pie(k, labels = domainExtensions)
- plot.legend(title = "domain_extensions:")
- plot.show()
+ objS = Statistics()
+ objS.staticQueryDetails()
+ objS.genDataSet(domainExtensions)
+ #objS.genDataSet(domainName)
+ 
+ #percent=(specific_result/total_result)*100
+ #print("Percent:{0:.2f}%".format(percent))
+
  
 main() 
