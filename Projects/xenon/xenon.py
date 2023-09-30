2cb9dc3e78c0d156dca246167f748ae0f1484449
Subhankar Ray <subhankarraypp32@gmail.com>
2023-09-30 10:14:54 +0530

    GUI-ViewWindow: Added
    Entry-Fields Required
    Also Authorization Frame to be generated

--- Projects/xenon/xenon.py
+++ Projects/xenon/xenon.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from abc import ABC , abstractmethod
 
 win=Tk()
@@ -27,25 +28,62 @@ class Win_frames(ABC):
     def frames_pos(self):
         pass
 
+    def frames_del(self,obj):
+        obj.destroy()
 
-class AuthWindow(Window,Win_frames):
+class Win_buttons(ABC):
+    @abstractmethod
+    def button_create(self ,f ,t ,bcl):
+        pass
+
+    def button_del(self,obj):
+        obj.destroy()
+
+class AuthWindow(Window,Win_frames,Win_buttons):
     def __init__(self):
-        Window().__init__()
-        Window().window_res()
+        super().__init__()
+        super().window_res()
+        self.frames_list=[]
     
 
     def frames_pos(self):
-        y= self.frames_gen(1,1,400,400)
-        y[0].pack(side="top", expand=False)
+        self.frames_list = self.frames_gen(1,1,400,400)
+        self.frames_list[0].pack(side="top", expand=False)
         print("Frame positioned")
-        Button(y[0], text="Sign In", bg="#FFFFFF", command= lambda: self.auth_script()).pack()
-
+        self.button_create(self.frames_list[0] ,"Sign In", "#FFFFFF")
+    
+    def button_create(self ,f ,t ,bcl):
+        btn1 = Button(f ,text=t ,bg=bcl ,command = lambda: self.auth_script())
+        btn1.pack()
+        Button(f, text="Delete_btn", bg =bcl, command = lambda: self.button_del(btn1)).pack()
 
     def auth_script(self):
         #Auth-Script
         print("Executing-Auth-Script")
-        pass
+        z = 1
+        if z:
+            print("Authenticated")
+            self.frames_del(self.frames_list[0])
+            ViewWindow().frames_pos()
 
+class ViewWindow(Window,Win_frames):
+    def __init__(self):
+        super().__init__()
+        super().window_res()
+
+    def frames_pos(self):
+        self.frames_list = self.frames_gen(1,1,400,400)
+        self.frames_list[0].pack(side="top", expand=False)
+        print("Frame positioned")
+        self.button_create(self.frames_list[0] ,"Password_Check", "#FFFFFF")
+
+    def button_create(self ,f ,t ,bcl):
+        btn1 = Button(f ,text=t ,bg=bcl ,command = lambda: self.pass_script())
+        btn1.pack()
+        Button(f, text="Delete_btn", bg =bcl, command = lambda: self.button_del(btn1)).pack()
+
+    def pass_script(self):
+        print("Password Policy Script")
 
 
 if __name__=="__main__":
